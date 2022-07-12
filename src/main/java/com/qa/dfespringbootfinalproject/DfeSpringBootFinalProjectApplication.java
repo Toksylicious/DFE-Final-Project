0bedc5773829bf686c6086b0957c5ea5cd7188a7
Toksylicious <tokeonas@outlook.com>
2022-07-12 12:03:10 +0100

    OA:First commit

---
+++ src/main/java/com/qa/dfespringbootfinalproject/DfeSpringBootFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringbootfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeSpringBootFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeSpringBootFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfespringbootfinalproject/DfeSpringBootFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringbootfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeSpringBootFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
